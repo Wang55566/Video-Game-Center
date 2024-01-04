ed5867bc9dc1411e1f3769a2f0369be7390fbbfa
Wang55566 <wang6@hawaii.edu>
2024-01-04 12:29:28 -0800

    darl mode

---
+++ game-hub/src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from '../assets/Logo.png';
+
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize='60px' />
+    </HStack>
+  )
+}
+
+export default NavBar
