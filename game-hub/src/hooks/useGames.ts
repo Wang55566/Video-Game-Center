bc44935b01c957453162c4b1339cd453a6690a04
Wang55566 <wang6@hawaii.edu>
2024-01-17 06:12:02 -0800

    morning

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -2,22 +2,9 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import useGameQueryStore from "../store";
 import { FetchResponse } from "./useData";
-import { Platform } from "./usePlatforms";
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
-
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  description_raw: string;
-  background_image: string;
-  parent_platforms: {
-    platform: Platform;
-  }[];
-  metacritic: number;
-  rating_top: number;
-}
 
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
--- game-hub/src/hooks/useGenres.ts
+++ game-hub/src/hooks/useGenres.ts
@@ -1,13 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 
 const useGenres = () =>
   useQuery({
