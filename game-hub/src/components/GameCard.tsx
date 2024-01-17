0f4b51fb3147074407cb200658cfc2ce0c2da09d
Wang55566 <wang6@hawaii.edu>
2024-01-17 05:59:41 -0800

    fetch game detail

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -1,27 +1,33 @@
-import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
-import { Game } from '../hooks/useGames'
-import PlatformIconList from './PlatformIconList'
-import CriticScore from './CriticScore'
-import getCroppedImageUrl from '../services/image-url'
-import Emoji from './Emoji'
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
+import Emoji from "./Emoji";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
-  game:Game
+  game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl(game.background_image)}/>
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <HStack justifyContent='space-between' marginBottom={3}>
-          <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top} /></Heading>
+        <Heading fontSize="2xl">
+          <Link to={"/games/" + game.slug}>{game.name}</Link>
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
--- game-hub/src/services/api-client.ts
+++ game-hub/src/services/api-client.ts
@@ -1,25 +1,31 @@
-import axios, { AxiosRequestConfig } from 'axios'
-import { FetchResponse } from '../hooks/useData';
+import axios, { AxiosRequestConfig } from "axios";
+import { FetchResponse } from "../hooks/useData";
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://api.rawg.io/api',
+  baseURL: "https://api.rawg.io/api",
   params: {
-    key: '3e806affe3434eb9aff1cdef267c0992'
-  }
-})
+    key: "3e806affe3434eb9aff1cdef267c0992",
+  },
+});
 
 class APIClient<T> {
   endpoint: string;
 
-  constructor(endpoint:string) {
+  constructor(endpoint: string) {
     this.endpoint = endpoint;
   }
 
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
-      .get<FetchResponse<T>>(this.endpoint,config)
-      .then(res => res.data);
-  }
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: string | number) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
