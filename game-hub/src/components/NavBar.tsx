71711dac3b91d1af748196c430d0fc5b676391eb
Wang55566 <wang6@hawaii.edu>
2024-02-05 03:11:01 -0800

    add readme

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 import { HStack, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import logo from "../assets/Logo.png";
-import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
